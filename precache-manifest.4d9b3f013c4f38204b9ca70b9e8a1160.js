2e30638cf125683c334ffc553630a64240d5d1d8
Jasleen Kaur <jasleen5900@gmail.com>
2021-06-18 18:21:58 +0530

    Updates

---
+++ precache-manifest.4d9b3f013c4f38204b9ca70b9e8a1160.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "573f35619e2ac987837b404acdc03220",
+    "url": "/path-finder/index.html"
+  },
+  {
+    "revision": "49b7993a4d5ee9f4943b",
+    "url": "/path-finder/static/css/2.44ebabcf.chunk.css"
+  },
+  {
+    "revision": "5ef4f8e897dfd73274f1",
+    "url": "/path-finder/static/css/main.f722abf9.chunk.css"
+  },
+  {
+    "revision": "49b7993a4d5ee9f4943b",
+    "url": "/path-finder/static/js/2.c81245fd.chunk.js"
+  },
+  {
+    "revision": "5ef4f8e897dfd73274f1",
+    "url": "/path-finder/static/js/main.e6647c7c.chunk.js"
+  },
+  {
+    "revision": "521120f9d3f3076ebb43",
+    "url": "/path-finder/static/js/runtime-main.57f57338.js"
+  }
+]);
